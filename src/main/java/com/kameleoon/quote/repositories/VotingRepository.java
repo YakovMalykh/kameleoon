261043558960dde9d964309ab56deed36ce79d67
YakovMalykh <to-yakoff@mail.ru>
2023-02-13 15:24:18 +0300

    созданы репозитории

---
+++ src/main/java/com/kameleoon/quote/repositories/VotingRepository.java
@@ -0,0 +1,9 @@
+package com.kameleoon.quote.repositories;
+
+import com.kameleoon.quote.models.Voting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VotingRepository extends JpaRepository<Voting, Long> {
+}
